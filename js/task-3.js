ba5c0d681b8b0d747a625337af3ef59cdc4009f6
Ihorsoft <153544183+Ihorsoft@users.noreply.github.com>
2024-07-21 16:20:04 +0300

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+function filterArray(numbers, value) { 
+    const apperNumbers = [];
+    for (const itemNumbers of numbers) {
+        if (itemNumbers > value) {
+           apperNumbers.push(itemNumbers);  
+         }     
+    }
+    return apperNumbers;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
